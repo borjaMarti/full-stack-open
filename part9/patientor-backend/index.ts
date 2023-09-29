f6a852f51d98b769de82e88cad974993960152b7
Borja Martí <borjamarti@outlook.com>
2023-09-29 19:12:34 +0200

    exercises 9.8-9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,6 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+const allowedOrigins = ['http://localhost:3000'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+app.use(cors(options));
+
 app.use(express.json());
 
 const PORT = 3005;
